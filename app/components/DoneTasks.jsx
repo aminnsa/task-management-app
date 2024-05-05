a7d30b497cb30b8204f05f8eb9e20554bcb4855f
Amin Sadeghzadeh <amin.sadeghzadeh1380@gmail.com>
2024-05-05 16:03:28 +0330

    Add Loading

--- app/components/DoneTasks.jsx
+++ app/components/DoneTasks.jsx
@@ -6,17 +6,20 @@ import TaskColumn from "./TaskColumn";
 
 function DoneTasks() {
   const [tasks, setTasks] = useState(null);
+  const [Loading, setLoading] = useState(false)
 
   useEffect(() => {
     handleFetchTasks();
   }, []);
 
   const handleFetchTasks = async () => {
+    setLoading(true)
     const response = await fetch(APIBaseURL);
     if (response.ok) {
       const responseBody = await response.json();
       setTasks(responseBody.filter((task) => task.isDone === true));
     }
+    setLoading(false)
   };
 
   return tasks ? (
