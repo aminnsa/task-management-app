a393a165a7664e21da89eff1010e839d0bc73646
Amin Sadeghzadeh <amin.sadeghzadeh1380@gmail.com>
2024-05-06 00:54:21 +0330

    task padding change

--- app/components/Task.jsx
+++ app/components/Task.jsx
@@ -10,7 +10,7 @@ function Task({title, desc, id, isDone, createdAt}) {
 
   
   return (
-    <div className='bg-gray-700 rounded-lg p-2 cursor-pointer' onClick={() => router.push(`/task/${id}`)} >
+    <div className='bg-gray-700 rounded-lg p-4 cursor-pointer' onClick={() => router.push(`/task/${id}`)} >
       <p className='text-xs text-gray-500 mb-5'>{new Date(createdAt).toLocaleString()}</p>
       <p className='font-bold text-white text-md'>{title}</p>
       <p className='text-sm text-gray-400'>{desc}</p>
