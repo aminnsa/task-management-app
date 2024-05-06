ecffff5761eb8e41bc4158ccde0c075d4fe2f9d9
Amin Sadeghzadeh <amin.sadeghzadeh1380@gmail.com>
2024-05-06 13:02:12 +0330

    fix styling

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="flex w-full items-center justify-center mt-10">
+    <div className="flex w-full items-center justify-center">
       <svg
         aria-hidden="true"
         class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
